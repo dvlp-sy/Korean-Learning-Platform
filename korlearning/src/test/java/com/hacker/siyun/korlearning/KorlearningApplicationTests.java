e0062a380fe7f214c40bc7836f50914669590ba9
dvlp-sy <sy030404@naver.com>
2024-01-07 16:43:19 +0900

    [add] Initialize Spring Boot

---
+++ korlearning/src/test/java/com/hacker/siyun/korlearning/KorlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacker.siyun.korlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorlearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
