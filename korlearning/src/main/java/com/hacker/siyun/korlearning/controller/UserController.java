deb6bf2c8f509ce524243291a0340115ff516113
dvlp-sy <sy030404@naver.com>
2024-01-08 12:10:36 +0900

    [feat] Implement getUserByUserId feature
    - 개별 유저 조회 기능 구현

--- korlearning/src/main/java/com/hacker/siyun/korlearning/controller/UserController.java
+++ korlearning/src/main/java/com/hacker/siyun/korlearning/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.hacker.siyun.korlearning.controller;
 
+import com.hacker.siyun.korlearning.common.ApiResponse;
+import com.hacker.siyun.korlearning.dto.UserDTO;
 import com.hacker.siyun.korlearning.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,10 @@ public class UserController
     private final UserService userService;
 
     public UserController(UserService userService) { this.userService = userService; }
+
+    @GetMapping("/users/{userId}")
+    public ApiResponse<UserDTO> getUserByUserId(@PathVariable Long userId)
+    {
+        return userService.getUserByUserId(userId);
+    }
 }
--- korlearning/src/main/java/com/hacker/siyun/korlearning/service/UserService.java
+++ korlearning/src/main/java/com/hacker/siyun/korlearning/service/UserService.java
@@ -1,7 +1,16 @@
 package com.hacker.siyun.korlearning.service;
 
+import com.hacker.siyun.korlearning.common.ApiResponse;
+import com.hacker.siyun.korlearning.common.exception.NotFoundException;
+import com.hacker.siyun.korlearning.common.response.ErrorMessage;
+import com.hacker.siyun.korlearning.common.response.SuccessMessage;
+import com.hacker.siyun.korlearning.dto.UserDTO;
+import com.hacker.siyun.korlearning.model.User;
 import com.hacker.siyun.korlearning.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.stream.Collector;
 
 @Service
 public class UserService
@@ -9,4 +18,12 @@ public class UserService
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) { this.userRepository = userRepository; }
+
+    public ApiResponse<UserDTO> getUserByUserId(Long userId)
+    {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.USER_NOT_FOUND));
+
+        return ApiResponse.success(SuccessMessage.GET_INDIVIDUAL_USER_SUCCESS, UserDTO.build(user));
+    }
 }
