4d9ad84deda11756376f0082f23565e23d3e4b99
dvlp-sy <sy030404@naver.com>
2024-01-08 11:19:23 +0900

    [feat] Implement getAllUsers Feature
    - 전체 유저 조회 기능 구현

--- korlearning/src/main/java/com/hacker/siyun/korlearning/controller/UserController.java
+++ korlearning/src/main/java/com/hacker/siyun/korlearning/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.hacker.siyun.korlearning.controller;
 
+import com.hacker.siyun.korlearning.common.ApiResponse;
+import com.hacker.siyun.korlearning.dto.UsersDTO;
 import com.hacker.siyun.korlearning.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController
@@ -9,4 +14,8 @@ public class UserController
     private final UserService userService;
 
     public UserController(UserService userService) { this.userService = userService; }
+
+    @GetMapping("/users")
+    public ApiResponse<List<UsersDTO>> getAllUsers() { return userService.getAllUsers(); }
+
 }
--- korlearning/src/main/java/com/hacker/siyun/korlearning/service/UserService.java
+++ korlearning/src/main/java/com/hacker/siyun/korlearning/service/UserService.java
@@ -1,7 +1,14 @@
 package com.hacker.siyun.korlearning.service;
 
+import com.hacker.siyun.korlearning.common.ApiResponse;
+import com.hacker.siyun.korlearning.common.response.SuccessMessage;
+import com.hacker.siyun.korlearning.dto.UsersDTO;
 import com.hacker.siyun.korlearning.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService
@@ -9,4 +16,13 @@ public class UserService
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) { this.userRepository = userRepository; }
+
+    public ApiResponse<List<UsersDTO>> getAllUsers()
+    {
+        List<UsersDTO> usersDTOList = userRepository.findAll().stream()
+                .map(UsersDTO::build)
+                .toList();
+
+        return ApiResponse.success(SuccessMessage.GET_ALL_USERS_SUCCESS, usersDTOList);
+    }
 }
