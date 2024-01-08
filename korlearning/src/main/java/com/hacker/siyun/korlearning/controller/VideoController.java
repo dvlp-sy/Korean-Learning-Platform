f4582fb8486c284d7c38498c94e73e2380525e2a
dvlp-sy <sy030404@naver.com>
2024-01-08 15:53:57 +0900

    [feat] Implement Upload Video Feature
    - 비디오 업로드 기능 구현

--- korlearning/src/main/java/com/hacker/siyun/korlearning/controller/VideoController.java
+++ korlearning/src/main/java/com/hacker/siyun/korlearning/controller/VideoController.java
@@ -1,6 +1,12 @@
 package com.hacker.siyun.korlearning.controller;
 
+import com.hacker.siyun.korlearning.common.ApiResponse;
+import com.hacker.siyun.korlearning.dto.UserRequestDTO;
+import com.hacker.siyun.korlearning.dto.VideoSummaryDTO;
 import com.hacker.siyun.korlearning.service.VideoService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +15,10 @@ public class VideoController
     private final VideoService videoService;
 
     public VideoController(VideoService videoService) { this.videoService = videoService; }
+
+    @PostMapping("/videos")
+    public ApiResponse<VideoSummaryDTO> uploadVideo(@RequestParam(name = "link") String link, @RequestBody UserRequestDTO userRequestDTO) throws Exception
+    {
+        return videoService.uploadVideo(link, userRequestDTO);
+    }
 }
