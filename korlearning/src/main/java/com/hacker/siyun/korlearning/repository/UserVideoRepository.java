d28dc1ee672bab40fcd8d9d5dce050049a3f4979
dvlp-sy <sy8389b@gmail.com>
2024-01-25 20:59:07 +0900

    [add] Update UserVideoRepository
    - 특정 userId에 해당하는 비디오 리스트를 찾아 리스트로 반환하는 함수 추가

--- korlearning/src/main/java/com/hacker/siyun/korlearning/repository/UserVideoRepository.java
+++ korlearning/src/main/java/com/hacker/siyun/korlearning/repository/UserVideoRepository.java
@@ -4,8 +4,10 @@ import com.hacker.siyun.korlearning.model.UserVideo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface UserVideoRepository extends JpaRepository<UserVideo, Long>
 {
-
+    public List<UserVideo> findAllByUser_UserId(Long userId);
 }
