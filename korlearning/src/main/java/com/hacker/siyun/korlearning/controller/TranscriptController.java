b19e631aa9f9c568c28fbcb1c80193c3487f7c6a
dvlp-sy <sy8389b@gmail.com>
2024-01-27 11:29:07 +0900

    [feat] Implement getAllTranscripts
    - 비디오 전체 자막 불러오기 기능 구현

--- korlearning/src/main/java/com/hacker/siyun/korlearning/controller/TranscriptController.java
+++ korlearning/src/main/java/com/hacker/siyun/korlearning/controller/TranscriptController.java
@@ -1,6 +1,11 @@
 package com.hacker.siyun.korlearning.controller;
 
+import com.hacker.siyun.korlearning.common.ApiResponse;
+import com.hacker.siyun.korlearning.dto.TranscriptsDTO;
+import com.hacker.siyun.korlearning.model.Transcript;
 import com.hacker.siyun.korlearning.service.TranscriptService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,10 @@ public class TranscriptController
     private final TranscriptService transcriptService;
 
     public TranscriptController(TranscriptService transcriptService) { this.transcriptService = transcriptService; }
+
+    @GetMapping("/videos/{videoId}/transcripts")
+    public ApiResponse<TranscriptsDTO> getAllTranscripts(@PathVariable("videoId") Long videoId)
+    {
+        return transcriptService.getAllTranscripts(videoId);
+    }
 }
--- korlearning/src/main/java/com/hacker/siyun/korlearning/service/TranscriptService.java
+++ korlearning/src/main/java/com/hacker/siyun/korlearning/service/TranscriptService.java
@@ -1,7 +1,17 @@
 package com.hacker.siyun.korlearning.service;
 
+import com.hacker.siyun.korlearning.common.ApiResponse;
+import com.hacker.siyun.korlearning.common.exception.NotFoundException;
+import com.hacker.siyun.korlearning.common.response.ErrorMessage;
+import com.hacker.siyun.korlearning.common.response.SuccessMessage;
+import com.hacker.siyun.korlearning.dto.TranscriptDTO;
+import com.hacker.siyun.korlearning.dto.TranscriptsDTO;
+import com.hacker.siyun.korlearning.model.Transcript;
+import com.hacker.siyun.korlearning.model.Video;
 import com.hacker.siyun.korlearning.repository.*;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TranscriptService
@@ -28,5 +38,21 @@ public class TranscriptService
         this.translationRepository = translationRepository;
         this.wordRepository = wordRepository;
         this.exampleRepository = exampleRepository;
+    }
+
+    public ApiResponse<TranscriptsDTO> getAllTranscripts(Long videoId)
+    {
+        videoRepository.findById(videoId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.VIDEO_NOT_FOUND));
+
+        List<TranscriptDTO> transcriptList = transcriptRepository.findAllByVideo_VideoId(videoId)
+                .stream()
+                .map(TranscriptDTO::build)
+                .toList();
+
+        if (transcriptList.isEmpty())
+            throw new NotFoundException(ErrorMessage.TRANSCRIPT_NOT_FOUND);
+
+        return ApiResponse.success(SuccessMessage.GET_TRANSCRIPTS_SUCCESS, new TranscriptsDTO(videoId, transcriptList));
     }
 }
