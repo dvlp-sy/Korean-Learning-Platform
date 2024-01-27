c1bc2acb9899cf26af50dcf665839580a99c7fa8
dvlp-sy <sy8389b@gmail.com>
2024-01-27 11:27:55 +0900

    [add] Create TranscriptDTO and TranscriptsDTO

---
+++ korlearning/src/main/java/com/hacker/siyun/korlearning/dto/TranscriptDTO.java
@@ -0,0 +1,34 @@
+package com.hacker.siyun.korlearning.dto;
+
+import com.hacker.siyun.korlearning.model.Transcript;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class TranscriptDTO
+{
+    private final Long transcriptId;
+    private final String sentence;
+    private final Double start;
+    private final Double duration;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private TranscriptDTO(Long transcriptId, String sentence, Double start, Double duration)
+    {
+        this.transcriptId = transcriptId;
+        this.sentence = sentence;
+        this.start = start;
+        this.duration = duration;
+    }
+
+    public static TranscriptDTO build(Transcript transcript)
+    {
+        return TranscriptDTO.builder()
+                .transcriptId(transcript.getTranscriptId())
+                .sentence(transcript.getSentence())
+                .start(transcript.getStart())
+                .duration(transcript.getDuration())
+                .build();
+    }
+}
