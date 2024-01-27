337e2016069201955aacbc24fbf7067c8404f8eb
dvlp-sy <sy8389b@gmail.com>
2024-01-27 11:28:33 +0900

    [add] Add findAllByVideo_VideoId

--- korlearning/src/main/java/com/hacker/siyun/korlearning/repository/TranscriptRepository.java
+++ korlearning/src/main/java/com/hacker/siyun/korlearning/repository/TranscriptRepository.java
@@ -4,8 +4,10 @@ import com.hacker.siyun.korlearning.model.Transcript;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface TranscriptRepository extends JpaRepository<Transcript, Long>
 {
-
+    public List<Transcript> findAllByVideo_VideoId(Long videoId);
 }
