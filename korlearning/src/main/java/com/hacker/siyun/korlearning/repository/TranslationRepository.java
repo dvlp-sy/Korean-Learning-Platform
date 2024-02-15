96d02cbb2cb0a730cbae6b8f849b3ba18cc1bbdb
dvlp-sy <sy8389b@gmail.com>
2024-02-15 17:26:32 +0900

    [feat] Implement saveTranslation
    - 번역 저장 기능 구현

--- korlearning/src/main/java/com/hacker/siyun/korlearning/repository/TranslationRepository.java
+++ korlearning/src/main/java/com/hacker/siyun/korlearning/repository/TranslationRepository.java
@@ -4,8 +4,10 @@ import com.hacker.siyun.korlearning.model.Translation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource
 public interface TranslationRepository extends JpaRepository<Translation, Long>
 {
-
+    public Optional<Translation> findByTranscript_TranscriptId(Long transcriptId);
 }
